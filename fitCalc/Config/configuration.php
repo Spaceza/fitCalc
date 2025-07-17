d4caba91f3b2dd4d21d8f2d4e69ba4aa7347abe0
Raphael Esposito <raphaelssposito@gmail.com>
2025-07-17 12:22:13 -0300

    Delete fitCalc directory

--- fitCalc/Config/configuration.php
+++
@@ -1,9 +0,0 @@
-<?php
-// CONFIGURAÇÃO DO BANCO DE DADOS
-
-define("DB_NAME", "fitcalc");
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASSWORD", '');
-define("DB_PORT", "3306");
-?>
